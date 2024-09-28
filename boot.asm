1e0264afa99c9926b2b3724707e5aab99bf60a2c
Strauberg <StefanStrauberg@gmail.com>
2024-09-28 10:26:17 +0300

    making bootloader

--- boot.asm
+++ boot.asm
@@ -1,37 +1,44 @@
-ORG 0
-BITS 16
+  ORG 0
+  BITS 16
+  _start:
+    jmp short start ; Short jump to label 'start'
+    nop             ; No operation, just a filler instruction
+    
+    times 33 db 0   ; Pad 33 bytes with zeros (to simulate the boot sector)
 
-jmp 0x7c0:start
+  start:
+    jmp 0x7c0:step2 ; Far jump to segment 0x7C0 at the label 'step2'
 
-start:
-  cli 
-  mov ax, 0x7c0   ; Устанавливает сегментные регистры DS и ES на адрес 0x7C0
-  mov ds, ax
-  mov es, ax
-  mov ax, 0x00    ; Устанавливает сегментный регистр SS на 0, чтобы стек начинался с адреса 0x0000
-  mov ss, ax
-  mov sp, 0x7c00  ;   Устанавливает указатель стека (SP) на 0x7C00
-  sti
-  mov si, message ; Загружаем адрес строки в регистр SI
-  call print      ; Вызываем процедуру для печати строки
-  jmp $           ; Бесконечный цикл
+  step2:
+    cli             ; Disable interrupts
+    mov ax, 0x7c0   ; Set Segment Registers DS and ES to segment 0x7C0 (0x7C00 = 0x7C0 * 16)
+    mov ds, ax
+    mov es, ax
+    mov ax, 0x00    ; Set Segment Register SS to 0, so that stack starts at address 0x0000
+    mov ss, ax
+    mov sp, 0x7c00  ; Set pointer of stack (SP) to 0x7C00
+    sti             ; Enable interrupts
 
-print:    
-.loop: 
-  lodsb           ; Загружаем байт из [SI] в AL и увеличиваем SI
-  cmp al, 0       ; Проверяем, является ли символ нулевым байтом
-  je .done        ; Если да, завершить печать
-  call print_char ; Вызов процедуры вывода символа
-  jmp .loop       ; Переход к следующему символу
-.done:
-  ret             ; Возврат из процедуры
+    mov si, message ; Load address of the message into register SI
+    call print      ; Call the print procedure
+    jmp $           ; Infinite loop
 
-print_char:
-  mov ah, 0eh     ; Функция вывода символа BIOS
-  int 0x10        ; Вызов прерывания BIOS для вывода символа
-  ret             ; Возврат из процедуры
+  print:    
+  .loop: 
+    lodsb           ; Load a byte from [SI] into AL and increment SI
+    cmp al, 0       ; Check if the character is a null terminator
+    je .done        ; If yes, jump to done
+    call print_char ; Call the print_char procedure
+    jmp .loop       ; Jump back to loop
+  .done:
+    ret             ; Return from the procedure
 
-message: db 'Hello World!', 0
+  print_char:
+    mov ah, 0eh     ; Set function for BIOS teletype output (0x0E)
+    int 0x10        ; Call BIOS interrupt 0x10 to output the character in AL
+    ret             ; Return from the procedure
 
-times 510-($ - $$) db 0
-dw 0xAA55
+  message: db 'Hello World!', 0
+
+  times 510-($ - $$) db 0
+  dw 0xAA55
